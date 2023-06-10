5b96e3bec51eede0408e66026d2d1e5e7c6392d0
ct-27 <ct27.it@gmail.com>
2023-06-11 02:50:05 +0530

    Created Intern Application Form and Succesful Applied Page

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./internform";
+import './style.css';
+
+function App() {
+
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+}
+
+export default App;

3a4dd337585bd7f323012a44cc083706eb4fa312
ARJUN PUNDIR <arjunpundir1106@gmail.com>
2023-06-10 23:57:45 +0530

    Second Commit

--- Team24/Team_24/models.py
+++ Team24/Team_24/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from rest_framework import serializers
 
 # Create your models here.
+
+class login(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=20)
+    
+
+    def __str__(self):
+        return self.username
+    
+class MyModelserializer(serializers.ModelSerializer):
+    class Meta:
+        model = login
+        fields = '__all__'
 
 
--- Team24/Team_24/views.py
+++ Team24/Team_24/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import login
+from rest_framework import status
+from rest_framework 
 
 # Create your views here.
+
+def index(request):
+    return render(request,"index.html")
+
+def intern_login(request):
+
 
 
